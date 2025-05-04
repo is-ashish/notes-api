6c23740ef3cb33cbdbd73c0ddc049cb039cb1cc6
Ashish Jain <ashishjnp28@gmail.com>
2025-05-04 16:52:24 +0530

    rebuild

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	// Welcome message printed when the server starts
 	fmt.Println("🚀 Welcome! Notes API server is starting...")
+
 	r := routes.SetupRoutes()
 	r.Run(":8080")
 }
